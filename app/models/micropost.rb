bb282a57d5778578d11a986959e1082f72aea1de
Matthew Bohrer <matthew.bohrer@gmail.com>
2012-03-07 14:46:39 -0500

    Calls alpha

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -28,48 +28,4 @@ class Micropost < ActiveRecord::Base
 	def content_and_tags
 		content + ", tags: "+description #NOTE: this is temp
 	end
-	
-	def report_info
-		tempstring = ""
-		if user.name.nil?
-			user.name='nil'
-		end
-		if user.email.nil?
-			user.email='nil'
-		end
-
-		if adf.nil?
-			adf="nil"
-		end
-		if content.nil?
-			content='nil'
-		end
-		if unit.nil?
-			unit='nil'
-		end
-		if category.nil?
-			category='nil'
-		end
-		if !event_date.nil?
-			eventstring = event_date.to_s
-		else
-			eventstring = "nil"
-		end
-		if !dob.nil?
-			dobstring = dob.to_s
-		else
-			dobstring="nil"
-		end
-		tempstring=""
-		tempstring+=eventstring+" | "
-		tempstring+=user.name+" | "
-		tempstring+=user.email+" | "
-		tempstring+=casenumber+" | "
-		tempstring+=adf+" | "
-		tempstring+=dobstring+" | "
-		tempstring+=content+" | "
-		tempstring+=unit+" | "
-		tempstring+=category+" | "
-		tempstring+=created_at.to_s
-	end
 end  

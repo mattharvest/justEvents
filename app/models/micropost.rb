78ad0707a55d616899814ad25e86183c60ed3cf0
Matthew Bohrer <matthew.bohrer@gmail.com>
2012-03-10 22:27:28 -0500

    bugfix form

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,9 @@
 class Micropost < ActiveRecord::Base
 
 	belongs_to :user
+	attr_accessible :defendant, :category, :casenumber, :event_date, :unit
 	has_and_belongs_to_many :tags
 	
-
 	casenum_regex = /CR[0-9]E[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]|JA-[0-9][0-9]-[0-9][0-9][0-9][0-9]|CJ[0-9][0-9][0-9][0-9][0-9][0-9]|CA[0-9][0-9][0-9][0-9][0-9][0-9]|CT[0-9][0-9][0-9][0-9][0-9][0-9]/i
 	
 	validates :content, :presence => true, :length => { :maximum => 255 }

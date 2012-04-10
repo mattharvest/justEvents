6dec883f39f87671cca291bdfa74d7e35cd19ce3
Matthew Bohrer <matthew.bohrer@gmail.com>
2012-04-10 13:02:09 -0400

    improve investigation assigment

--- app/controllers/investigations_controller.rb
+++ app/controllers/investigations_controller.rb
@@ -79,7 +79,7 @@ class InvestigationsController < ApplicationController
 			check_achievements
 			flash[:investigationsuccess]="Investigation created"
 			#now create the ToDo for the assignee to review the case within 7 days
-			assignee_todo = @assignee.todoitems.build(:duedate=>Date.today+7, :casenumber=>@casefile.lead_casenumber, :user_id=>@investigation.assignee_id, :content=>'Review this case for SIU')
+			assignee_todo = @assignee.todoitems.build(:duedate=>Date.today+7, :casenumber=>@casefile.lead_casenumber, :user_id=>@investigation.assignee_id, :content=>'Review this case for '+current_user.unit)
 				#insert the email notice HERE
 			if assignee_todo.save
 				flash[:investigationtodosuccess]="ToDo for assignee created"
@@ -91,7 +91,7 @@ class InvestigationsController < ApplicationController
 			UserMailer.investigation_notice(@assignee, @investigation).deliver
 			
 			#finally, create the event to reflect that it's been assigned
-			investigation_post = current_user.microposts.build(:event_date=>Date.today.to_s, :content=>'New investigation started by '+current_user.name+', assigned to '+@assignee.name, :defendant=>@investigation.defendant, :category=>'investigation', :casenumber=>@casefile.lead_casenumber)
+			investigation_post = current_user.microposts.build(:unit=>@assignee.unit, :event_date=>Date.today.to_s, :content=>'New investigation started by '+current_user.name+', assigned to '+@assignee.name, :defendant=>@investigation.defendant, :category=>'investigation', :casenumber=>@casefile.lead_casenumber)
 			if investigation_post.save
 				flash[:investigationpostsuccess]="Assignment of investigation posted"
 			else
